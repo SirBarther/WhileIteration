2d0a84ec4d32638d679fa5824e4298a6725428dc
Michael <Michael@DESKY>
2016-05-31 18:54:34 -0400

    Lesson completed.

--- WhileIteration/Program.cs
+++ WhileIteration/Program.cs
@@ -10,7 +10,88 @@ namespace WhileIteration
     {
         static void Main(string[] args)
         {
-
+            bool displayMenu = true;
+            while (displayMenu == true)
+            {
+                displayMenu = MainMenu();
+            }
+            
         }
+
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Play the numbers game");
+            Console.WriteLine("2) Play the guessing game");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if (result == "1")
+            {
+                PrintNumbers();
+                return true;
+            }
+
+            else if (result == "2")
+            {
+                GuessingGame();
+                return true;
+            }
+
+            else if (result == "3")
+            {
+                Console.WriteLine("Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Unknown answer! Please try again!");
+                Console.WriteLine();
+                return true;
+            }
+        }
+
+        private static void PrintNumbers()
+        {
+            Console.Clear();
+            Console.WriteLine("Print Numbers!");
+            Console.WriteLine("Type a number: ");
+            int result = int.Parse(Console.ReadLine());
+            int counter = 1;
+            while (counter < result + 1)
+            {
+                Console.Write(counter);
+                Console.Write("-");
+                counter++;
+            }
+            Console.ReadLine();
+        }
+
+        private static void GuessingGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Guessing Game!");
+
+            Random myRandom = new Random();
+            int randomNumber = myRandom.Next(1, 11);
+
+            int guesses = 0;
+            bool incorrect = true;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10: ");
+                string result = Console.ReadLine();
+                guesses++;
+                if (result == randomNumber.ToString())
+                    incorrect = false;
+                else
+                    Console.WriteLine("Wrong!");
+            } while (incorrect);
+            Console.WriteLine("Correct! It took {0} guesses.", guesses);
+
+            Console.ReadLine();
+        }       
+        
     }
 }
